8f81477e57a23fb8fce0f6df0f339d32764bfacc
Okumans <kaopancraft@gmail.com>
2025-02-21 02:15:35 +0700

    feat: add company schema

---
+++ src/types/Company.d.ts
@@ -0,0 +1,9 @@
+import { Document } from "mongoose";
+
+interface Company extends Document {
+    name: string;
+    address: string;
+    website: string;
+    description: string;
+    tel: string;
+}
