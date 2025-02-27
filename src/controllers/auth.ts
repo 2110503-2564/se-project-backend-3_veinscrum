94fba0666febbfb1e49d409de6b892df48cd5b18
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2025-02-28 00:34:40 +0700

    feat: implement logout

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -105,6 +105,22 @@ export const login = async (
     }
 };
 
+/// @desc     Logout user
+/// @route    GET /api/v1/auth/logout
+/// @access   Private
+export const logout = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void> => {
+    res.cookie("token", "none", {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({ success: true, data: {} });
+};
+
 /// @desc     Get current logged in user
 /// @route    GET /api/v1/auth/me
 /// @access   Private
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { getMe, login, register } from "@/controllers/auth";
+import { getMe, login, register, logout } from "@/controllers/auth";
 import { protect } from "@/middleware/auth";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.get("/logout", protect, logout);
 
 export { router as authRouter };
