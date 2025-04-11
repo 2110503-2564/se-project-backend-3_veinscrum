29b1e31ae91376c57b7619b0fae3a03cbc6dbe5f
Sirawit Petchsriudom <6733275621@student.chula.ac.th>
2025-04-12 00:07:13 +0700

    feat: add getJobListingByCompany, remove getJobListings

--- src/routes/companies.ts
+++ src/routes/companies.ts
@@ -6,6 +6,7 @@ import {
     updateCompany,
 } from "@/controllers/companies";
 import { getInterviewSessionsByCompany } from "@/controllers/interviewSessions";
+import { getJobListingByCompany } from "@/controllers/jobListing";
 import { authorize, protect } from "@/middleware/auth";
 import { Router } from "express";
 
@@ -14,19 +15,26 @@ const router = Router();
 router
     .route("/")
     .get(getCompanies)
-    .post(protect, authorize("admin","company"), createCompany);
+    .post(protect, authorize("admin", "company"), createCompany);
 
 router
     .route("/:id")
     .get(getCompany)
-    .put(protect, authorize("admin","company"), updateCompany)
-    .delete(protect, authorize("admin","company"), deleteCompany);
+    .put(protect, authorize("admin", "company"), updateCompany)
+    .delete(protect, authorize("admin", "company"), deleteCompany);
 
 router.get(
     "/:id/sessions",
     protect,
     authorize("user", "admin", "company"),
     getInterviewSessionsByCompany,
+);
+
+router.get(
+    "/:id/job-listings",
+    protect,
+    authorize("user", "admin", "company"),
+    getJobListingByCompany,
 );
 
 export { router as companiesRouter };
