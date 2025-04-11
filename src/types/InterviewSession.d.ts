f1ef4ce85146dec1ed1c19ec1505f3c63d1729e0
Sirawit Petchsriudom <6733275621@student.chula.ac.th>
2025-04-11 23:43:25 +0700

    feat: add jobListingRouter to server

--- src/types/InterviewSession.d.ts
+++ src/types/InterviewSession.d.ts
@@ -2,6 +2,7 @@ import { Document } from "mongoose";
 
 interface InterviewSession extends Document {
     jobListing: ObjectId;
+    company: ObjectId;
     user: ObjectId;
     date: Date;
     createdAt: Date;
