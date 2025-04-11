959a43a44770e19df6cbdb7ba105cf27967587b8
Sirawit Petchsriudom <6733275621@student.chula.ac.th>
2025-04-11 17:37:20 +0700

    feat: add jobListing and owner to Company schema

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -34,6 +34,13 @@ const CompanySchema = new mongoose.Schema<Company>(
                 "Please add a valid phone number in the format XXX-XXX-XXXX",
             ],
         },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: [true, "Please add company owner"],
+        },
+        jobListing: {
+            type: [mongoose.Schema.Types.ObjectId]
+        }
     },
     {
         toJSON: { virtuals: true },
