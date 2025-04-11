66f26032c75f2e07f7139e21f7b66692026277cf
Sirawit Petchsriudom <6733275621@student.chula.ac.th>
2025-04-11 23:38:53 +0700

    feat: add jobListing route

--- src/controllers/companies.ts
+++ src/controllers/companies.ts
@@ -1,6 +1,5 @@
 import { CompanyModel } from "@/models/Company";
 import { InterviewSessionModel } from "@/models/InterviewSession";
-import { UserModel } from "@/models/User";
 import { RequestWithAuth } from "@/types/Request";
 import { buildComparisonQuery } from "@/utils/buildComparisonQuery";
 import { filterAndPaginate } from "@/utils/filterAndPaginate";
@@ -82,17 +81,6 @@ export async function createCompany(
     next: NextFunction,
 ) {
     try {
-        // Check if user existed and is company
-        const owner = await UserModel.findById(req.body.owner);
-
-        if (!owner || owner.role != "company") {
-            res.status(400).json({
-                success: false,
-                message: "Bad owner information",
-            });
-            return;
-        }
-
         const company = await CompanyModel.create(req.body);
         res.status(201).json({ success: true, data: company });
     } catch (err) {
