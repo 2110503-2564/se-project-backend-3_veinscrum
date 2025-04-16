9e59aa1dcdadbbe01459fb8263123d1ef8f223ed
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2025-04-16 15:06:00 +0700

    Feat: Add Docker Deployment (#3)
    * chore: remove dist
    
    * feat: add build + docker build
    
    * fix: dont use 80
    
    * chore: remove traefik and use internal dokploy traefik

--- dist/src/constants/queryFields.js
+++
@@ -1,1 +0,0 @@
-export const EXCLUDED_QUERY_FIELDS = ["select", "sort", "page", "limit"];
--- dist/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-import { getMe, login, register, logout } from "../controllers/auth.js";
-import { protect } from "../middleware/auth.js";
-const router = express.Router();
-router.post("/register", register);
-router.post("/login", login);
-router.get("/me", protect, getMe);
-router.get("/logout", protect, logout);
-export { router as authRouter };
