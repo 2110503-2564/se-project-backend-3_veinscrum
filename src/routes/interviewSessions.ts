488c88127b3e8f461af033b5d55b69341c14d25c
Okumans <kaopancraft@gmail.com>
2025-04-18 23:50:03 +0700

    fix: update authorization roles for creating, updating, and deleting interview sessions

--- src/routes/interviewSessions.ts
+++ src/routes/interviewSessions.ts
@@ -13,13 +13,21 @@ const router = Router({ mergeParams: true });
 router
     .route("/")
     .get(protect, getInterviewSessions)
-    .post(protect, authorize("admin", "user"), createInterviewSession);
+    .post(
+        protect,
+        authorize("admin", "user", "company"),
+        createInterviewSession,
+    );
 
 router
     .route("/:id")
     .get(protect, getInterviewSession)
-    .put(protect, authorize("admin", "user"), updateInterviewSession)
-    .delete(protect, authorize("admin", "user"), deleteInterviewSession);
+    .put(protect, authorize("admin", "user", "company"), updateInterviewSession)
+    .delete(
+        protect,
+        authorize("admin", "user", "company"),
+        deleteInterviewSession,
+    );
 
 export { router as interviewSessionsRouter };
 
