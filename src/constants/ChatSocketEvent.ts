05ee4ef905e4e0dd85f1146e5e89cee400789db2
Okumans <kaopancraft@gmail.com>
2025-04-22 20:48:26 +0700

    feat: add ChatSocketEvent enum for chat-related socket events

---
+++ src/constants/ChatSocketEvent.ts
@@ -0,0 +1,7 @@
+export enum ChatSocketEvent {
+    ChatMessage = "chat-message",
+    ChatHistory = "chat-history",
+    ChatUpdated = "chat-updated",
+    ChatDeleted = "chat-deleted",
+    ChatError = "chat-error",
+}
