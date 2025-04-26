4e309b22d64f9cdfa47c249de67987976ad1df24
CLOUD <6733288821@student.chula.ac.th>
2025-04-26 23:50:48 +0700

    fix: delete gatFlags in route

--- src/routes/flag.ts
+++ src/routes/flag.ts
@@ -1,10 +1,9 @@
-import { createFlag, deleteFlag, getFlags } from "@/controllers/flag";
+import { createFlag, deleteFlag} from "@/controllers/flag";
 import { authorize, protect } from "@/middleware/auth";
 import * as express from "express";
 
 const router = express.Router();
 
-router.get("/", protect, authorize("admin", "company"), getFlags);
 router.post("/", protect, authorize("admin", "company"), createFlag);
 router.delete("/:id", protect, authorize("admin", "company"), deleteFlag);
 
